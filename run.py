bf6f7674be4c149f2daa1dafde0af218179ff8d4
Lasserini <lasserini@gmail.com>
2021-10-26 12:51:45 +0000

    Import random functionality and create first wave of global variables

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ What global variables do I need?
 - grid (something that represents our grid)
 - grid_size (how large a grid is used)
 - number_of_ships (how many ships to place)
-- missiles_left (how many shots left before game is lost)
+- shots_left (how many shots left before game is lost)
 - ships_sunk (something to track fully sunk ships, to provide player info on their progress and to track if game is won)
 
 
@@ -18,6 +18,8 @@ What functions do I need?
 def start_game  (once user confirms/inputs difficulty settings, this runs and builds the grid with ships)
 
 def hit_miss  (check if user has hit a ship, and change icon accordingly)
+    def hit
+    def miss
 
 
 
@@ -28,3 +30,30 @@ Legend for Grid:
 "X" = Hit (damaged ship)
 "0" = Miss (water that has been shot at, without hitting a ship)
 """
+
+# Imports used to generate random ship placement, time to help ensure randomness.
+    # random.seed(time.time()) -> makes the random seed time dependant, to ensure new random seed for additional games. Used in create_grid.
+import random
+import time
+
+"""
+The program utilized a fair amount of global variables, they are listed here.
+"""
+# A variable for the grid upon which the ships are places
+grid = [[]]
+
+# Variable with letters, used to create the grid later.
+letters = "ABCDEFGHIJKL"
+
+# Variable for the size of the grid.
+grid_size = 10
+
+# Variable for how many turns remain before the game is lost.
+shots_left = 50
+
+# Variable for number of ships
+ship_count = 8
+
+# Variable that tracks amount of ships sunk.
+ships_sunk = 0
+
